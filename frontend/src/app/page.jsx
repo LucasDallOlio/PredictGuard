b1a148b32cd51985820f356acfe2606ef2bf1106
PHELIPE BELO SILVEIRA <25170080@educ123.sp.senai.br>
2026-03-02 09:44:34 -0300

    Add login

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,10 @@
+import LoginForm from "@/components/shadcn-space/blocks/login-01/login";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <LoginForm></LoginForm>
+    </>
+  );
+}
