7b96ab971f119f4a9c50308d2d591d0beea1bd05
HELOISA SOARES GOMES <25170403@educ123.sp.senai.br>
2026-03-16 10:32:37 -0300

    Arrumando página tabela

--- frontend/src/components/shadcn-studio/table/table-13.jsx
+++ frontend/src/components/shadcn-studio/table/table-13.jsx
@@ -55,7 +55,7 @@ export default function UsersTable() {
   }
 
   return (
-    <div className="w-full max-w-6xl mx-auto py-10 space-y-6">
+    <div className="w-full max-w-7xl mx-auto py-10 space-y-6">
 
       {/* Título */}
       <div>
