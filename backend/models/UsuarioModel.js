1d20b745e2bedbcfee1a00660d6e24052d9502c4
Vitor Santos <vitor.oliveira2005@hotmail.com.br>
2026-04-04 13:16:18 -0300

    feat: implementa CRUD no model do usuário

---
+++ backend/models/UsuarioModel.js
@@ -0,0 +1,48 @@
+import { create, read, update, deleteRecord } from '../config/database.js';
+import { hashPassword } from '../utils/bcrypt.js';
+
+class UsuarioModel{
+    
+    static async buscarPorID(id) {
+        try{
+            const rows = await read('usuarios', `id = ${Number(id)}`);
+            return rows[0] || null;
+        }
+        catch(error){
+            throw new Error(`Erro ao buscar usuario por ID: ${error.message}`);
+        }
+    }
+
+    static async criar(usuario){
+        usuario.senha = await hashPassword(usuario.senha)
+        try{
+            return await create('usuarios', usuario);
+        }
+        catch(error){
+            throw new Error(`Erro ao criar usuario: ${error.message}`);
+        }
+    }
+
+    static async atualizar(id, dadosUsuario){
+        try{
+            if(dadosUsuario.senha){
+                dadosUsuario.senha = await hashPassword(dadosUsuario.senha)
+            }
+            return await update('usuarios', dadosUsuario, 'id = ?', [id])
+        }
+        catch(error){
+            throw new Error(`Erro ao atualizar usuario: ${error.message}`);
+        }
+    }
+
+    static async excluir(id){
+        try{
+            return await deleteRecord('usuarios', 'id = ?', [id])
+        }
+        catch(error){
+            throw new Error(`Erro ao excluir usuario: ${error.message}`);
+        }
+    }
+}
+
+export default UsuarioModel
